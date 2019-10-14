96795ce570fc88531b6344da34a689104b682250
zhou <624061566@qq.com>
2019-10-14 15:47:25 +0800

    error handle

--- projects/error/src/main.rs
+++ projects/error/src/main.rs
@@ -1,17 +1,40 @@
-use std::fs::File;
-use std::io::ErrorKind;
+// use std::fs::File;
+// use std::io::ErrorKind;
 fn main() {
-    let f = File::open("hello.txt");
+    // let f = File::open("hello.txt");
 
-    let f = match f {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => panic!("Tried to create file but there was a problem: {:?}", e),
-            },
-            other_error => panic!("There was a problem opening the file: {:?}", other_error),
-        },
-    };
+    // let f = match f {
+    //     Ok(file) => file,
+    //     Err(error) => match error.kind() {
+    //         ErrorKind::NotFound => match File::create("hello.txt") {
+    //             Ok(fc) => fc,
+    //             Err(e) => panic!("Tried to create file but there was a problem: {:?}", e),
+    //         },
+    //         other_error => panic!("There was a problem opening the file: {:?}", other_error),
+    //     },
+    // };
     //println!("Hello, world!");
+}
+
+use std::fs::File;
+use std::io;
+use std::io::Read;
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    // let f = File::open("hello.txt");
+
+    // let mut f = match f {
+    //     Ok(file) => file,
+    //     Err(e) => return Err(e),
+    // };
+
+    // let mut s = String::new();
+
+    // match f.read_to_string(&mut s) {
+    //     Ok(_) => Ok(s),
+    //     Err(e) => Err(e),
+    // }
+    let mut s = String::new();
+    let f = File::open(("hello.txt"))?.read_to_string(&mut s)?;
+    Ok(s)
 }
