c7a56b3fa0893cdf48c1ed4e5dbb0520e5e3dce0
zhou <624061566@qq.com>
2019-10-20 10:26:22 +0800

    ch13 over

---
+++ projects/closures_iterators/target/debug/closures_iterators.d
@@ -0,0 +1,1 @@
+/home/zhou/Documents/Rust/projects/closures_iterators/target/debug/closures_iterators: /home/zhou/Documents/Rust/projects/closures_iterators/src/main.rs
---
+++ projects/closures_iterators/target/debug/deps/closures_iterators-736a2a2aed650308.d
@@ -0,0 +1,5 @@
+/home/zhou/Documents/Rust/projects/closures_iterators/target/debug/deps/closures_iterators-736a2a2aed650308: src/main.rs
+
+/home/zhou/Documents/Rust/projects/closures_iterators/target/debug/deps/closures_iterators-736a2a2aed650308.d: src/main.rs
+
+src/main.rs:
