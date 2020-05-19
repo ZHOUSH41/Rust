9d3429d561f70a69324aeb46fe22e52f40ab2ad1
zhou <624061566@qq.com>
2020-05-19 17:29:42 +0800

    proj1 part3

--- pna-rust/kvs/src/lib.rs
+++ pna-rust/kvs/src/lib.rs
@@ -1,19 +1,24 @@
 use std::option::Option;
-pub struct KvStore ;
+use std::collections::HashMap;
+pub struct KvStore {
+    store: HashMap<String, String>
+}
 impl KvStore {
     pub fn new() -> KvStore {
-        panic!();
+        KvStore {
+            store: HashMap::new()
+        }
     }
 
-    pub fn set<F>(&self, _arg1: F, _arg2: F) {
-        panic!();
+    pub fn set(&mut self, key: String, value: String) {
+        self.store.insert(key, value);
     }
 
-    pub fn get<F>(&self, _arg1: F) -> Option<F>{
-        unimplemented!();
+    pub fn get(&self, key: String) -> Option<String>{
+        self.store.get(&key).cloned()
     }
 
-    pub fn remove<F>(&self, _arg1: F) {
-        panic!();
+    pub fn remove(&mut self, key: String) {
+        self.store.remove(&key);
     }
 }
