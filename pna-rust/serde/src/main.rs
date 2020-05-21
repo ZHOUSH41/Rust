3d2c75ffc3ff75f28c487922e4f3a3986e5d63f9
zhou <624061566@qq.com>
2020-05-21 16:01:28 +0800

    json serialization

--- pna-rust/serde/src/main.rs
+++ pna-rust/serde/src/main.rs
@@ -1,3 +1,25 @@
+use serde::{Serialize, Deserialize};
+use std::fs::File;
+use serde_json;
+use std::io::{Write, Read};
+
+
+#[derive(Serialize, Deserialize, Debug)]
+ struct Move {
+     step: u32,
+     direction: String,
+ }
+
 fn main() {
-    println!("Hello, world!");
+    let a = Move{ step: 100,direction: String::from("left")};
+    let serialize = serde_json::to_string(&a).unwrap();
+    println!("serialize = {:?}", a);
+    let mut file = File::create("output.json").expect("create failed!");
+    file.write_all(serialize.as_bytes()).expect("write failed!");
+
+    let mut file = File::open("output.json").expect("cannot open file!");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("cannot read");
+    let deserialize: Move = serde_json::from_str(&contents).unwrap();
+    println!("deserialize = {:?}", deserialize);
 }
