b348fc5627fb20c7d8cda3d40c4e135d3fc305d9
zhou <624061566@qq.com>
2020-05-23 16:05:59 +0800

    proj2 part1

--- pna-rust/kvs/src/lib.rs
+++ pna-rust/kvs/src/lib.rs
@@ -2,6 +2,10 @@
 //! A simple key/value store
 
 use std::collections::HashMap;
+use std::result;
+use std::io;
+use std::path::PathBuf;
+
 /// The `KvStore` stores string key/value pairs.
 /// Key/value pairs are stored in a `Hashmap` in memory and not persisted to disk/
 ///
@@ -18,31 +22,46 @@ pub struct KvStore {
     store: HashMap<String, String>,
 }
 
+/// Custom error type
+#[derive(Debug)]
+pub enum Error {
+    /// Io error
+    Io(io::Error)
+}
+
+/// Custom result type
+pub type Result<T> = result::Result<T, Error>;
 /// method of KvStore
 impl KvStore {
     /// Creates a `KvStore`
-    pub fn new() -> KvStore {
-        KvStore {
+    pub fn open(path: impl Into<PathBuf>) -> Result<KvStore> {
+        Ok(KvStore {
             store: HashMap::new(),
-        }
+        })
     }
 
     /// Sets the value of a string key to a string.
-    ///
-    /// If the key already exists, the previous value will be overwritten.
-    pub fn set(&mut self, key: String, value: String) {
+    /// Returns `Ok` if success,
+    /// Returns `Err` if failure.
+    pub fn set(&mut self, key: String, value: String) -> Result<()>{
         self.store.insert(key, value);
+        panic!()
     }
 
     /// Gets the string value of a given string key.
-    ///
-    /// Returns `None` if the given key does not exist.
-    pub fn get(&self, key: String) -> Option<String> {
-        self.store.get(&key).cloned()
+    /// Returns `Ok(Some)` when getting a existent key,
+    /// Returns `Ok(None)` when getting a non-existent key,
+    /// Retruns `Err` when error
+    pub fn get(&self, key: String) -> Result<Option<String>> {
+        self.store.get(&key).cloned();
+        panic!()
     }
 
-    /// Remove a given key.
-    pub fn remove(&mut self, key: String) {
+    /// Return `Ok(Some)` previously stored value when removing a existent key,
+    /// return `Ok(None)` when removing a non-existent key,
+    /// return `Err` when error
+    pub fn remove(&mut self, key: String) -> Result<Option<String>>{
         self.store.remove(&key);
+        panic!()
     }
 }
