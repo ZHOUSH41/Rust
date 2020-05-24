a717aba2f46f303a9990d875cab21e4f6ffef6ac
zhou <624061566@qq.com>
2020-05-24 17:30:51 +0800

    proj2 part2-7

--- pna-rust/kvs/src/bin/kvs.rs
+++ pna-rust/kvs/src/bin/kvs.rs
@@ -1,13 +1,15 @@
-extern crate clap;
 use clap::{App, AppSettings, Arg, SubCommand};
+use kvs::{KvStore, KvsError, Result};
+use std::env::current_dir;
 use std::process::exit;
-fn main() {
+
+fn main() -> Result<()> {
     let matches = App::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
         .setting(AppSettings::DisableHelpSubcommand)
-        .setting(AppSettings::SubcommandRequired)
+        .setting(AppSettings::SubcommandRequiredElseHelp)
         .setting(AppSettings::VersionlessSubcommands)
         .subcommand(
             SubCommand::with_name("set")
@@ -21,29 +23,48 @@ fn main() {
         )
         .subcommand(
             SubCommand::with_name("get")
-                .about("Get the value of a string key to a string")
+                .about("Get the string value of a given string key")
                 .arg(Arg::with_name("KEY").help("A string key").required(true)),
         )
         .subcommand(
             SubCommand::with_name("rm")
-                .about("Remove the value of a string key")
+                .about("Remove a given key")
                 .arg(Arg::with_name("KEY").help("A string key").required(true)),
         )
         .get_matches();
 
     match matches.subcommand() {
-        ("set", Some(_matches)) => {
-            eprintln!("unimplemented");
-            exit(1);
+        ("set", Some(matches)) => {
+            let key = matches.value_of("KEY").expect("KEY argument missing");
+            let value = matches.value_of("VALUE").expect("VALUE argument missing");
+
+            let mut store = KvStore::open(current_dir()?)?;
+            store.set(key.to_string(), value.to_string())?;
         }
-        ("get", Some(_matches)) => {
-            eprintln!("unimplemented");
-            exit(1);
+        ("get", Some(matches)) => {
+            let key = matches.value_of("KEY").expect("KEY argument missing");
+
+            let mut store = KvStore::open(current_dir()?)?;
+            if let Some(value) = store.get(key.to_string())? {
+                println!("{}", value);
+            } else {
+                println!("Key not found");
+            }
         }
-        ("rm", Some(_matches)) => {
-            eprintln!("unimplemented");
-            exit(1);
+        ("rm", Some(matches)) => {
+            let key = matches.value_of("KEY").expect("KEY argument missing");
+
+            let mut store = KvStore::open(current_dir()?)?;
+            match store.remove(key.to_string()) {
+                Ok(()) => {}
+                Err(KvsError::KeyNotFound) => {
+                    println!("Key not found");
+                    exit(1);
+                }
+                Err(e) => return Err(e),
+            }
         }
         _ => unreachable!(),
     }
+    Ok(())
 }
--- pna-rust/kvs/src/lib.rs
+++ pna-rust/kvs/src/lib.rs
@@ -1,67 +1,8 @@
 #![deny(missing_docs)]
-//! A simple key/value store
+//! A simple key/value store.
 
-use std::collections::HashMap;
-use std::result;
-use std::io;
-use std::path::PathBuf;
+pub use error::{KvsError, Result};
+pub use kv::KvStore;
 
-/// The `KvStore` stores string key/value pairs.
-/// Key/value pairs are stored in a `Hashmap` in memory and not persisted to disk/
-///
-/// Example:
-/// ``` rust
-/// # use kvs::KvStore;
-/// let mut store = KvStore::new();
-/// store.set("key".to_owned(), "value".to_owned());
-/// let val = store.get("key".to_owned());
-/// assert_eq!(val, Some("value".to_owned()));
-/// ```
-#[derive(Default)]
-pub struct KvStore {
-    store: HashMap<String, String>,
-}
-
-/// Custom error type
-#[derive(Debug)]
-pub enum Error {
-    /// Io error
-    Io(io::Error)
-}
-
-/// Custom result type
-pub type Result<T> = result::Result<T, Error>;
-/// method of KvStore
-impl KvStore {
-    /// Creates a `KvStore`
-    pub fn open(path: impl Into<PathBuf>) -> Result<KvStore> {
-        Ok(KvStore {
-            store: HashMap::new(),
-        })
-    }
-
-    /// Sets the value of a string key to a string.
-    /// Returns `Ok` if success,
-    /// Returns `Err` if failure.
-    pub fn set(&mut self, key: String, value: String) -> Result<()>{
-        self.store.insert(key, value);
-        panic!()
-    }
-
-    /// Gets the string value of a given string key.
-    /// Returns `Ok(Some)` when getting a existent key,
-    /// Returns `Ok(None)` when getting a non-existent key,
-    /// Retruns `Err` when error
-    pub fn get(&self, key: String) -> Result<Option<String>> {
-        self.store.get(&key).cloned();
-        panic!()
-    }
-
-    /// Return `Ok(Some)` previously stored value when removing a existent key,
-    /// return `Ok(None)` when removing a non-existent key,
-    /// return `Err` when error
-    pub fn remove(&mut self, key: String) -> Result<Option<String>>{
-        self.store.remove(&key);
-        panic!()
-    }
-}
+mod error;
+mod kv;
